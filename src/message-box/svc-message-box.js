60d585d233a25c63f83927a5eb959566d39bb96b
Francisco Vallarino <fjvallarino@users.noreply.github.com>
2017-03-01 19:16:11 +0200

    Add Message Box component (#68)
    * Add Message Box component
    
    * Add message box service utility

---
+++ src/message-box/svc-message-box.js
@@ -0,0 +1,25 @@
+"use strict";
+
+angular.module("risevision.common.components.message-box.services", [])
+  .factory("messageBox", ["$q", "$log", "$modal", "$templateCache",
+    function ($q, $log, $modal, $templateCache) {
+      return function (title, message, close) {
+        var modalInstance = $modal.open({
+          template: $templateCache.get("message-box/message-box.html"),
+          controller: "messageBoxInstance",
+          windowClass: "modal-custom",
+          resolve: {
+            title: function () {
+              return title;
+            },
+            message: function () {
+              return message;
+            },
+            button: function () {
+              return close || "common.close";
+            }
+          }
+        });
+      };
+    }
+  ]);
