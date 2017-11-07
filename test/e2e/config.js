83fc1604809b7b6e3782fd27040ba744f9c3da8c
Alex Deaconu <alex.deaconu@gmail.com>
2017-11-07 18:05:09 -0500

    Remove unneeded config file

--- test/e2e/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-angular.module("risevision.common.i18n.config", [])
-  .constant("LOCALES_PREFIX", "/bower_components/rv-common-i18n/dist/locales/translation_")
-  .constant("LOCALES_SUFIX", ".json");
